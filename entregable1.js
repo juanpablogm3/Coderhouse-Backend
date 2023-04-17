3ad453cfa60d8bb44d7e2f6702b1f5492cb271a4
Juan Pablo Garcia Martinez <juanpablogm3@hotmail.com>
2023-04-16 21:45:20 -0300

    Primer desafío entregable

--- entregable1.js
+++ entregable1.js
@@ -1,8 +1,8 @@
 class ProductManager{
     constructor(){
         this.products=[];
-        this.idManager= 0;
-    }
+        this.idManager= 1;
+    };
 
     addProduct(newTitle, newDescription, newPrice, newThumbnail, newCode, newStock){
         const newProduct={
@@ -13,40 +13,43 @@ class ProductManager{
             thumbnail: newThumbnail,
             code: newCode,
             stock: newStock
-        }
+        };
         
         const existingProduct = this.products.find((p) => p.code === newCode);
-        if (existingProduct === undefined) {
-            this.products.push(newProduct)
-            this.idManager++;
-        }else{
-            console.log(`El producto con código ${newCode} ya existe`);
-        }
+        if(existingProduct){
+            return `El producto con código ${newCode} ya existe`;
+        };
+        if(!newTitle || !newDescription || !newPrice || !newThumbnail || !newCode || !newStock){
+            return 'Se deben completar todos los campos';
+        };
 
-        
-    }
+        this.products.push(newProduct)
+        this.idManager++;
+        return 'Producto agregado con éxito';
+    };
 
     getProducts(){
         return this.products;
-    }
+    };
 
 
     getProductById(id) {
         const productById = this.products.find((elem) => elem.id === id);
-        if (productById !== undefined) {
-            return productById;
-        } else {
+        if (!productById) {
             return `Producto con id ${id} no existe`;
         }
-    }
-}
+        return productById;
+    };
+};
 
 // *********** TESTING **************
 const productManager = new ProductManager();
 
 console.log(productManager.getProducts());
-productManager.addProduct("producto prueba","Este es un producto prueba",200,"sin imagen","abc123",25);
+console.log(productManager.addProduct("producto prueba","Este es un producto prueba",200,"sin imagen","abc123",25));
+console.log(productManager.addProduct("Este es un producto prueba",200,"sin imagen","abc1234",25)); // PRODUCTO SIN UN CAMPO
+console.log(productManager.addProduct("producto prueba","Este es un producto prueba",200,"sin imagen","abc1234",25)); 
+console.log(productManager.addProduct("producto prueba","Este es un producto prueba",200,"sin imagen","abc123",25)); // PRODUCTO CON CODIGO REPETIDO
 console.log(productManager.getProducts());
-productManager.addProduct("producto prueba","Este es un producto prueba",200,"sin imagen","abc123",25);
-console.log(productManager.getProductById(0));
-console.log(productManager.getProductById(66));
+console.log(productManager.getProductById(1));
+console.log(productManager.getProductById(66)); // ID INEXISTENTE
