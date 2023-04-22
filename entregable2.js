f177a61bb40d0668830feac41d57ddb21baaa25e
Juan Pablo Garcia Martinez <juanpablogm3@hotmail.com>
2023-04-21 23:16:21 -0300

    correcciones menores

--- entregable2.js
+++ entregable2.js
@@ -11,9 +11,9 @@ class ProductManager{
         try{
             const prodsStr = fs.readFileSync(this.path, "utf-8");
             this.products = JSON.parse(prodsStr);
-            //return this.products;
+            return this.products;
         } catch(err){
-            console.log(`El archivo no existe: ${err}`);
+            return `El archivo no existe: ${err}`;
         }
     };
 
@@ -21,15 +21,16 @@ class ProductManager{
         const prodStr = JSON.stringify(this.products, null, 2);
         try{
             fs.writeFileSync(this.path, prodStr);
-            return 'Guardado con éxito';
+            return "Archivo guardado con éxito";
         } catch(err) {
-            Console.log(`Error al escribir el archivo: ${err}`)
+            return `Error al escribir el archivo: ${err}`;
         }
     };
             
     addProduct(newTitle, newDescription, newPrice, newThumbnail, newCode, newStock){
         
         this.readFile();
+
         const newProduct={
             title: newTitle,
             description: newDescription,
@@ -48,11 +49,14 @@ class ProductManager{
         };
         newProduct.id = this.idManager;
         this.products.push(newProduct);
+        this.saveFile();
         this.idManager++;
+        return `Producto agregado con éxito`;
+
     };
 
     getProducts(){
-        this.readFile();
+       return this.readFile();
     };
 
 
@@ -80,8 +84,7 @@ class ProductManager{
                 return `Error en el campo a modificar: ${field}`;
             }
             this.products[indexToUpdate][field] = newValue;
-            const prodStr = JSON.stringify(this.products, null, 2);
-            fs.writeFileSync(this.path, prodStr);
+            this.saveFile();
             return `Producto actualizado con éxito`;
         } catch(err){
             console.log(`Error en update ${err}`);
@@ -99,26 +102,25 @@ class ProductManager{
             this.saveFile();
             return `Producto con id ${id} eliminado con éxito`;
         }catch(err){
-            console.log(`Elproducto no existe: ${err}`);
+            console.log(`El producto no existe: ${err}`);
         };
     };        
 };
 
-// *********** TESTING **************
 
+// *********** TESTING **************
 const productManager = new ProductManager("productos.json");
-console.log(productManager.getProducts());
-console.log(productManager.addProduct("producto prueba1","Este es un producto prueba",200,"sin imagen","abc123",25));
+console.log(productManager.getProducts()); // Sin estar creado el array de productos
+console.log(productManager.addProduct("producto prueba1","Este es un producto prueba",200,"sin imagen","abc123",25)); // con ID 1
 console.log(productManager.addProduct("Este es un producto prueba",200,"sin imagen","abc1234",25)); // PRODUCTO SIN UN CAMPO
-console.log(productManager.addProduct("producto prueba2","Este es un producto prueba",200,"sin imagen","abc1234",25)); 
+console.log(productManager.addProduct("producto prueba2","Este es un producto prueba",200,"sin imagen","abc1234",25)); // con ID 2
 console.log(productManager.addProduct("producto prueba","Este es un producto prueba",200,"sin imagen","abc123",25)); // PRODUCTO CON CODIGO REPETIDO
 console.log(productManager.deleteProduct(2));
 console.log(productManager.deleteProduct(5)); // PRODUCTO NO EXISTE
 console.log(productManager.getProductById(1));
-console.log(productManager.getProductById(2)); // ID INEXISTENTE (BORRADO)
+console.log(productManager.getProductById(5)); // ID INEXISTENTE
 console.log(productManager.updateProduct(1,"title","super zapatilla"));
 console.log(productManager.updateProduct(1,"prize",1000)); // CAMPO MAL INGRESADO
 console.log(productManager.updateProduct(5,"price",1000)); // ID NO EXISTE
-console.log(productManager.addProduct("producto prueba2","Este es un producto prueba",200,"sin imagen","abc123456",25)); 
+console.log(productManager.addProduct("producto prueba2","Este es un producto prueba",200,"sin imagen","abc123456",25)); // Para demostrar id (ID 3) incrementado
 console.log(productManager.getProducts());
-
