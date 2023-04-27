1d1413c2148a833834276b8004508308e2817b9c
Juan Pablo Garcia Martinez <juanpablogm3@hotmail.com>
2023-04-27 13:19:26 -0300

    tomando forma

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
 import express from "express";
-import productManager from "productManager.js";
+import ProductManager from './productManager.js';
 
-const app = express()
-const port = 3000
-const prodManager = new productManager("../data/products.json")
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const port = 8080;
+const productManager = new ProductManager("products_old.json");
 
+// *********** TESTING **************
+/* async function test() {
+    console.log(await productManager.getProducts()); // Sin estar creado el array de productos
+    console.log(await productManager.addProduct("producto prueba1","Este es un producto prueba",200,"sin imagen","abc123",25)); // con ID 1
+    console.log(await productManager.addProduct("Este es un producto prueba",200,"sin imagen","abc1234",25)); // PRODUCTO SIN UN CAMPO
+    console.log(await productManager.addProduct("producto prueba2","Este es un producto prueba",200,"sin imagen","abc1234",25)); // con ID 2
+    console.log(await productManager.addProduct("producto prueba3","Este es un producto prueba",200,"sin imagen","abc12345",25)); // con ID 3
+    console.log(await productManager.addProduct("producto prueba4","Este es un producto prueba",200,"sin imagen","abc12346",25)); // con ID 4
+    console.log(await productManager.addProduct("producto prueba5","Este es un producto prueba",200,"sin imagen","abc12347",25)); // con ID 5
+    console.log(await productManager.addProduct("producto prueba6","Este es un producto prueba",200,"sin imagen","abc12348",25)); // con ID 6
+    console.log(await productManager.addProduct("producto prueba7","Este es un producto prueba",200,"sin imagen","abc12349",25)); // con ID 7
+    console.log(await productManager.addProduct("producto prueba","Este es un producto prueba",200,"sin imagen","abc123",25)); // PRODUCTO CON CODIGO REPETIDO
+    console.log(await productManager.deleteProduct(2));
+    console.log(await productManager.deleteProduct(22)); // PRODUCTO NO EXISTE
+    console.log(await productManager.getProductById(1));
+    console.log(await productManager.getProductById(22)); // ID INEXISTENTE
+    console.log(await productManager.updateProduct(1,"title","super zapatilla"));
+    console.log(await productManager.updateProduct(1,"prize",1000)); // CAMPO MAL INGRESADO
+    console.log(await productManager.updateProduct(22,"price",1000)); // ID NO EXISTE
+    console.log(await productManager.addProduct("producto prueba8","Este es un producto prueba",200,"sin imagen","abc123456",25)); // Para demostrar que usa ids mínimos disponibles
+    console.log(await productManager.getProducts());
+}
+  
+test(); */
 
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
@@ -13,7 +39,7 @@ console.log(`Example app listening on port ${port}`)
 app.get("/products/:pid", (req, res)=> {
     const idProd = req.params.pid;
     res.json(
-        prodManager.getProductById(idProd)
+        productManager.getProductById(idProd)
     )
 })
 
@@ -23,8 +49,6 @@ app.get("/products", (req, res)=> {
         const allProducts = prodManager.getProducts();
         res.json(allProducts.slice(0,limit));
     } else {
-        re.json(prodManager.getProducts());
+        res.json(productManager.getProducts());
     }
 })
-
-// prueba github
--- src/productManager.js
+++ src/productManager.js
@@ -1,152 +1,117 @@
-import fs from 'fs';
+import { promises as fs } from 'fs';
 
-class ProductManager{
-    constructor(path){
-        this.path = path;
-        this.products=[];
-        this.idManager = [];
-    };
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+    this.products = [];
+    this.idManager = [];
+  }
 
-    async readFile(){
-        try{
-            const prodsStr =  await fs.promises.readFile(this.path, "utf-8");
-            this.products = JSON.parse(prodsStr);
-            return this.products;
-        } catch(err){
-            return `El archivo no existe: ${err}`;
-        }
-    };
-
-    async saveFile(){
-        const prodStr = JSON.stringify(this.products, null, 2);
-        try{
-            await fs.promises.writeFile(this.path, prodStr);
-            return "Archivo guardado con éxito";
-        } catch(err) {
-            return `Error al escribir el archivo: ${err}`;
-        }
-    };
-
-    async getNextId() {
-        const newId = await new Promise((resolve) => {
-            const nextId = this.idManager.length > 0 ? Math.min(...this.idManager) : 1;
-            let availableId = nextId;
-            while (this.idManager.includes(availableId)) {
-                availableId++;
-            }
-            resolve(availableId);
-        });
-        return newId;
+  async readFile() {
+    try {
+      const prodsStr = await fs.readFile(this.path, 'utf-8');
+      this.products = JSON.parse(prodsStr);
+      return this.products;
+    } catch (err) {
+      return `El archivo no existe: ${err}`;
     }
-        
+  }
 
-            
-    async addProduct(newTitle, newDescription, newPrice, newThumbnail, newCode, newStock){
-        
+  async saveFile() {
+    const prodStr = JSON.stringify(this.products, null, 2);
+    try {
+      await fs.writeFile(this.path, prodStr);
+      return 'Archivo guardado con éxito';
+    } catch (err) {
+      return `Error al escribir el archivo: ${err}`;
+    }
+  }
+
+  async addProduct(newTitle, newDescription, newPrice, newThumbnail, newCode, newStock) {
+    await this.readFile();
+
+    const newProduct = {
+      title: newTitle,
+      description: newDescription,
+      price: newPrice,
+      thumbnail: newThumbnail,
+      code: newCode,
+      stock: newStock
+    };
+
+    const existingProduct = this.products.find(p => p.code === newCode);
+    if (existingProduct) {
+      return `El producto con código ${newCode} ya existe`;
+    }
+    if (!newTitle || !newDescription || !newPrice || !newThumbnail || !newCode || !newStock) {
+      return 'Se deben completar todos los campos';
+    }
+
+    const nextId = this.idManager.length > 0 ? Math.min(...this.idManager) : 1;
+    let newId = nextId;
+    while (this.idManager.includes(newId)) {
+      newId++;
+    }
+    newProduct.id = newId;
+    this.products.push(newProduct);
+    this.idManager.push(newId);
+    this.idManager.sort((a, b) => a - b);
+    this.products.sort((a, b) => a.id - b.id);
+    await this.saveFile();
+    return `Producto agregado con éxito`;
+  }
+
+  async getProducts() {
+    return await this.readFile();
+  }
+
+  async getProductById(id) {
+    try {
+      await this.readFile();
+      const productById = this.products.find(elem => elem.id === id);
+      if (!productById) {
+        return `Producto con id ${id} no existe`;
+      }
+      return productById;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async updateProduct(id, field, newValue) {
+    try {
+      await this.readFile();
+      const indexToUpdate = this.products.findIndex(elem => elem.id === id);
+      if (indexToUpdate === -1) {
+        return `No existe un producto con id ${id}`;
+      }
+      if (field !== ('title' || 'description' || 'price' || 'thumbnail' || 'code' || 'stock')) {
+        return `Error en el campo a modificar: ${field}`;
+      }
+      this.products[indexToUpdate][field] = newValue;
+      await this.saveFile();
+      return `Producto actualizado con éxito`;
+    } catch (err) {
+      console.log(`Error en update ${err}`);
+    }
+  }
+
+  async deleteProduct(id){
+    try{
         await this.readFile();
-
-        const newProduct={
-            title: newTitle,
-            description: newDescription,
-            price: newPrice,
-            thumbnail: newThumbnail,
-            code: newCode,
-            stock: newStock
-        };
-        
-        const existingProduct = this.products.find((p) => p.code === newCode);
-        if(existingProduct){
-            return `El producto con código ${newCode} ya existe`;
-        };
-        if(!newTitle || !newDescription || !newPrice || !newThumbnail || !newCode || !newStock){
-            return 'Se deben completar todos los campos';
-        };
-
-        const newId = await this.getNextId();
-        newProduct.id = newId;
-        this.idManager.push(newId);
-        this.products.push(newProduct);
-        this.idManager.sort((a,b)=>a-b);
-        this.products.sort((a,b)=>a.id-b.id);
+        const filteredProds = this.products.filter((elem)=> elem.id !== id )
+        if (filteredProds.length === this.products.length){
+            return `No extiste producto con id ${id}`;
+        }
+        this.products = filteredProds;
         await this.saveFile();
-        return `Producto agregado con éxito`;
-
+        const idIndex = this.idManager.indexOf(id);
+        this.idManager.splice(idIndex,1);
+        return `Producto con id ${id} eliminado con éxito`;
+    }catch(err){
+        console.log(`El producto no existe: ${err}`);
     };
-
-    async getProducts(){
-       await this.readFile();
-       return this.products.length>0 ? this.products : `No hay productos`;
-    };
-
-
-    async getProductById(id) {
-        try{
-           await this.readFile();
-            const productById = this.products.find((elem) => elem.id === id);
-            if (!productById) {
-                return `Producto con id ${id} no existe`;
-            }
-            return productById;
-        } catch(err){
-            console.log(err);
-        }
-    };
-    
-    async updateProduct(id, field, newValue){
-        try{
-            await this.readFile();
-            const indexToUpdate = this.products.findIndex(elem => elem.id === id);
-            if (indexToUpdate===-1){
-                return `No existe un producto con id ${id}`;
-            }
-            if(field!==("title" || "description" || "price" || "thumbnail" || "code" || "stock")){
-                return `Error en el campo a modificar: ${field}`;
-            }
-            this.products[indexToUpdate][field] = newValue;
-            await this.saveFile();
-            return `Producto actualizado con éxito`;
-        } catch(err){
-            console.log(`Error en update ${err}`);
-        }
-    }
-
-    async deleteProduct(id){
-        try{
-            await this.readFile();
-            const filteredProds = this.products.filter((elem)=> elem.id !== id )
-            if (filteredProds.length === this.products.length){
-                return `No extiste producto con id ${id}`;
-            }
-            this.products = filteredProds;
-            await this.saveFile();
-            const idIndex = this.idManager.indexOf(id);
-            this.idManager.splice(idIndex,1);
-            return `Producto con id ${id} eliminado con éxito`;
-        }catch(err){
-            console.log(`El producto no existe: ${err}`);
-        };
-    };        
+};        
 };
 
-
-// *********** TESTING **************
-/* const productManager = new ProductManager("productos.json");
-console.log(productManager.getProducts()); // Sin estar creado el array de productos
-console.log(productManager.addProduct("producto prueba1","Este es un producto prueba",200,"sin imagen","abc123",25)); // con ID 1
-console.log(productManager.addProduct("Este es un producto prueba",200,"sin imagen","abc1234",25)); // PRODUCTO SIN UN CAMPO
-console.log(productManager.addProduct("producto prueba2","Este es un producto prueba",200,"sin imagen","abc1234",25)); // con ID 2
-console.log(productManager.addProduct("producto prueba3","Este es un producto prueba",200,"sin imagen","abc12345",25)); // con ID 3
-console.log(productManager.addProduct("producto prueba4","Este es un producto prueba",200,"sin imagen","abc12346",25)); // con ID 4
-console.log(productManager.addProduct("producto prueba5","Este es un producto prueba",200,"sin imagen","abc12347",25)); // con ID 5
-console.log(productManager.addProduct("producto prueba6","Este es un producto prueba",200,"sin imagen","abc12348",25)); // con ID 6
-console.log(productManager.addProduct("producto prueba7","Este es un producto prueba",200,"sin imagen","abc12349",25)); // con ID 7
-console.log(productManager.addProduct("producto prueba","Este es un producto prueba",200,"sin imagen","abc123",25)); // PRODUCTO CON CODIGO REPETIDO
-console.log(productManager.deleteProduct(2));
-console.log(productManager.deleteProduct(22)); // PRODUCTO NO EXISTE
-console.log(productManager.getProductById(1));
-console.log(productManager.getProductById(22)); // ID INEXISTENTE
-console.log(productManager.updateProduct(1,"title","super zapatilla"));
-console.log(productManager.updateProduct(1,"prize",1000)); // CAMPO MAL INGRESADO
-console.log(productManager.updateProduct(22,"price",1000)); // ID NO EXISTE
-console.log(productManager.addProduct("producto prueba8","Este es un producto prueba",200,"sin imagen","abc123456",25)); // Para demostrar que usa ids mínimos disponibles
-console.log(productManager.getProducts()); */
+export default ProductManager;
