963e8c4847458a0692c6ae3b5d31e7beb16b988d
Juan Pablo <juanpablogm3@hotmail.com>
2023-04-29 21:43:11 -0300

    CRUD

--- src/app.js
+++ src/app.js
@@ -14,15 +14,51 @@ console.log(`Example app listening on port ${port}`)
 app.get("/products/:pid", async (req, res)=> {
     const idProd = parseInt(req.params.pid);
     const reqProd = await productManager.getProductById(idProd);
-    return res.json(reqProd)
+    if(typeof reqProd !== 'object'){
+        return res
+            .status(400)
+            .json({error: 'Id not found'})
+    }
+    return res
+        .status(200)
+        .json(reqProd)
 })
 
 app.get("/products", async (req, res)=> {
     const limit = parseInt(req.query.limit);
     const allProducts = await productManager.getProducts();
     if (!limit){
-        return res.json(allProducts);
+        return res
+            .status(200)
+            .json(allProducts);
     } else {
-        return res.json(allProducts.slice(0,limit));
+        return res
+            .status(200)
+            .json(allProducts.slice(0,limit));
     }
 })
+
+app.put("/products/:pid", async (req, res)=> {
+    const idProd = parseInt(req.params.pid);
+    const { field, newValue } = req.body;
+    await productManager.updateProduct(idProd, field, newValue);
+    return res
+        .status(200)
+        .json({Message: "Product updated"})
+})
+
+app.delete("/products/:pid", async (req, res)=> {
+    const idProd = parseInt(req.params.pid);
+    await productManager.deleteProduct(idProd);
+    return res
+        .status(200)
+        .json({message: 'Product deleted'})
+})
+
+app.post("/products", async (req, res)=> {
+    productManager.addProduct(req.body);
+    return res
+        .status(201)
+        .json({message: "Product created!"})
+})
+
