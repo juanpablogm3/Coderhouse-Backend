bced40bf40d3fad30d44829038790d24d01fb271
Juan Pablo Garcia Martinez <juanpablogm3@hotmail.com>
2023-10-19 01:40:08 -0300

    carrito 2

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -8,7 +8,7 @@ cartsRouter.get('/:cid', cartController.getCartById);
 cartsRouter.put('/:cid', cartController.replaceProductsInCart);
 cartsRouter.put('/:cid/products/:pid', cartController.modifyProductQuantity);
 cartsRouter.post('/:cid/products/:pid', (isUser||isPremium), cartController.addProductToCart); //quien puede comprar
-cartsRouter.delete('/:cid/products/:pid', isUser, cartController.removeProductFromCart);
+cartsRouter.delete('/:cid/products/:pid', (isUser||isPremium), cartController.removeProductFromCart);
 cartsRouter.delete('/:cid', cartController.deleteProductsInCart);
 cartsRouter.delete('/:cid/products/:pid/units', cartController.removeProductFromCartByUnit);
 cartsRouter.delete('/:cid', cartController.deleteCartById);
